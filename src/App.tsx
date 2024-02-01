30f6e3b9b5825fe61cd0783c47a2f898b0b0ee1c
Khadija <zaidikhadija462@gmail.com>
2024-02-02 00:52:12 +0500

    Styling completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import QuestionCard from "./components/QuestionCard";
 import { fetchQuizQuestions } from "./Api";
 import { QuestionsState, Difficulty } from "./Api";
+import { GlobalStyle } from "./App.styles";
+import { Wrapper } from "./App.styles";
 export type AnswerObject = {
   question: string;
   answer: string;
@@ -42,14 +44,14 @@ function App() {
       const correct = questions[number]?.correct_answer === answer;
       if (correct) {
         setScore((prev) => prev + 1);
-        const answerObject = {
-          question: questions[number]?.question,
-          answer,
-          correct,
-          correctAnswer: questions[number]?.correct_answer,
-        };
-        setUserAnswers((prev) => [...prev, answerObject]);
       }
+      const answerObject = {
+        question: questions[number]?.question,
+        answer,
+        correct,
+        correctAnswer: questions[number]?.correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
     }
   };
   const nextQuestion = () => {
@@ -62,34 +64,37 @@ function App() {
     }
   };
   return (
-    <div className="App">
-      <h1>REACT QUIZ</h1>
-      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-        <button className="start" onClick={startTrivia}>
-          Start
-        </button>
-      ) : null}
-      {!gameOver && <p className="score">Score: </p>}
-      {loading && <p>Loading Questions ...</p>}
-      {!loading && !gameOver && (
-        <QuestionCard
-          questionNum={number + 1}
-          totalQuestions={TOTAL_QUESTIONS}
-          question={questions[number]?.question}
-          answers={questions[number]?.answers}
-          userAnswer={userAnswers ? userAnswers[number] : undefined}
-          callback={checkAnswer}
-        />
-      )}
-      {!gameOver &&
-      !loading &&
-      userAnswers.length === number + 1 &&
-      number !== TOTAL_QUESTIONS - 1 ? (
-        <button className="next" onClick={nextQuestion}>
-          Next Question
-        </button>
-      ) : null}
-    </div>
+    <>
+      <GlobalStyle />
+      <Wrapper className="App">
+        <h1>REACT QUIZ</h1>
+        {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+          <button className="start" onClick={startTrivia}>
+            Start
+          </button>
+        ) : null}
+        {!gameOver && <p className="score">Score: {score} </p>}
+        {loading && <p>Loading Questions ...</p>}
+        {!loading && !gameOver && (
+          <QuestionCard
+            questionNum={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number]?.question}
+            answers={questions[number]?.answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
+        {!gameOver &&
+        !loading &&
+        userAnswers.length === number + 1 &&
+        number !== TOTAL_QUESTIONS - 1 ? (
+          <button className="next" onClick={nextQuestion}>
+            Next Question
+          </button>
+        ) : null}
+      </Wrapper>
+    </>
   );
 }
 
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AnswerObject } from "../App";
+import { Wrapper, ButtonWrapper } from "./QuestionCard.styles";
 type Props = {
   question: string;
   answers: string[];
@@ -17,14 +18,18 @@ const QuestionCard: React.FC<Props> = ({
   totalQuestions,
 }) => {
   return (
-    <div>
+    <Wrapper>
       <p className="number">
         Question : {questionNum} / {totalQuestions}
       </p>
       <p dangerouslySetInnerHTML={{ __html: question }}></p>
       <div>
         {answers?.map((answer) => (
-          <div key={answer}>
+          <ButtonWrapper
+            correct={userAnswer?.correctAnswer === answer}
+            userClicked={userAnswer?.answer === answer}
+            key={answer}
+          >
             <button
               disabled={userAnswer ? true : false}
               value={answer}
@@ -32,10 +37,10 @@ const QuestionCard: React.FC<Props> = ({
             >
               <span dangerouslySetInnerHTML={{ __html: answer }} />
             </button>
-          </div>
+          </ButtonWrapper>
         ))}
       </div>
-    </div>
+    </Wrapper>
   );
 };
 export default QuestionCard;
